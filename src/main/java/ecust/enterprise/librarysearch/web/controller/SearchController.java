fa41796b7ad0f99ad1d602d9d55f8fccfc0c91ed
zuoshiyu <35421121+zuoshiyu@users.noreply.github.com>
2022-11-30 19:28:38 +0800

    LS-1 Implemented Hotword Search

--- src/main/java/ecust/enterprise/librarysearch/web/controller/SearchController.java
+++ src/main/java/ecust/enterprise/librarysearch/web/controller/SearchController.java
@@ -1,34 +1,31 @@
 package ecust.enterprise.librarysearch.web.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.EnumSet;
-import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ecust.enterprise.librarysearch.business.services.DigitalBookService;
+import ecust.enterprise.librarysearch.business.services.HotWordService;
 import ecust.enterprise.librarysearch.business.services.PhysicalBookService;
+import ecust.enterprise.librarysearch.business.util.Filter;
 import ecust.enterprise.librarysearch.business.util.ListWrapper;
 import ecust.enterprise.librarysearch.business.util.TextFilter;
-import ecust.enterprise.librarysearch.business.util.Filter;
 
 @Controller
 public class SearchController
 {
   @Autowired
   private PhysicalBookService physicalBookService;
+  @Autowired
+  private DigitalBookService digitalBookService;
+  @Autowired
+  private HotWordService hotWordService;
 
   @GetMapping("/simplesearch")
   public ModelAndView simpleSearch(String keyword, Filter filter)  {
@@ -42,6 +39,7 @@ public class SearchController
     {
       mav.addObject("physicalBooks",
           physicalBookService.getBySimpleSearch(keyword, filter));
+      hotWordService.update(keyword);
     }
     return mav;
   }
@@ -70,6 +68,7 @@ public class SearchController
         new ListWrapper<String>(
             new ArrayList<String>(EnumSet.allOf(Filter.class)
                 .stream().map(Filter::getValue).toList())));
+    hotWordService.update(keyword);
     
     return mav;
   }
@@ -86,9 +85,29 @@ public class SearchController
     else 
     {
       mav.addObject("physicalBooks", physicalBookService.getByTextSearch(keyword, textFilter));
+      hotWordService.update(keyword);
     }
     mav.addObject("textFilters", EnumSet.allOf(TextFilter.class));
 
+    return mav;
+  }
+  
+  @GetMapping("/hotwordsearch")
+  public ModelAndView hotwordSearch(String keyword)
+  {
+    ModelAndView mav = new ModelAndView("hotword-search-view");
+    
+    if (keyword == null)
+    {
+      mav.addObject("physicalBooks", physicalBookService.getAll());
+    } 
+    else
+    {
+      mav.addObject("physicalBooks", physicalBookService.getByKeyword(keyword));
+      hotWordService.update(keyword);
+    }
+    mav.addObject("hotwords", hotWordService.getWithinMonth());
+    
     return mav;
   }
 }
